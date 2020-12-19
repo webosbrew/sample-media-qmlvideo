712e3eb01af74776d13c7f41314a8f6ea495863e
Mariotaku Lee <mariotaku.lee@gmail.com>
2020-12-20 07:17:27 +0900

    Simplified build config

--- cmake/WebOS-SDK.cmake
+++
@@ -1,7 +0,0 @@
-# QMake is not yet included in SDK itself, that one we needed is located in build-webos/BUILD/sysroots/x86_64-linux
-set(OE_QMAKE_PATH_EXTERNAL_HOST_BINS $ENV{OE_QMAKE_PATH_HOST_BINS})
-
-# This is required to make build work
-list(APPEND CMAKE_PREFIX_PATH "$ENV{OECORE_TARGET_SYSROOT}/usr/lib/cmake/Qt5")
-
-add_definitions(-DQ_OS_WEBOS)
